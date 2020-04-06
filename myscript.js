72deb8cf2127647757d66f6bdaeb9cbb29772564
Anurag_tekale <60124075+anurag-tekale@users.noreply.github.com>
2020-04-06 11:55:56 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,21 @@
+//navbar links
+
+const navbar = document.querySelector(".navbar");
+      a=navbar.querySelectorAll("a");
+
+      a.forEach(function(element){
+          element.addEventListener("click",function(){
+              for(let i=0; i<a.length;i++){
+                  a[i].classList.remove("active");
+              }
+              this.classList.add("active")
+              document.querySelector(".navbar").classList.toggle("show");
+          })
+      })
+//anurag
+
+const anurag=document.querySelector(".anurag");
+
+anurag.addEventListener("click",function(){
+    document.querySelector(".navbar").classList.toggle("show");
+})
